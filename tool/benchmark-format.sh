fc31624f1809a6266649bc6539c638ec62896de9
kktk-KO <kktkko3579@gmail.com>
2016-09-02 00:39:01 +0900

    add unit converter

--- tool/benchmark-format.sh
+++ tool/benchmark-format.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-cat "${1}-profile-raw.csv" | grep "\"" | sed -e "s/,/ /g" | sed -e 's/^"[^\/]*\/\([0-9]\+\)"/\1/' > ${1}-profile.csv
+cat "${1}-profile-raw.csv" | grep "\"" | sed -e "s/,/ /g" | sed -e 's/^"[^\/]*\/\([0-9]\+\)"/\1/' | sed -e 's/k/000/' | sed -e 's/M/000000/' > ${1}-profile.csv
