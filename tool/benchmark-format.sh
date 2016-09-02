015af1a9e3b3bca83c3174c76994ea0deab80f74
kktk-KO <kktkko3579@gmail.com>
2016-09-02 10:16:50 +0900

    fix

--- tool/benchmark-format.sh
+++ tool/benchmark-format.sh
@@ -1,3 +1,10 @@
 #!/bin/sh
 
-cat "${1}-profile-raw.csv" | grep "\"" | sed -e "s/,/ /g" | sed -e 's/^"[^\/]*\/\([0-9]\+\)"/\1/' | sed -e 's/k/000/' | sed -e 's/M/000000/' > ${1}-profile.csv
+cat "${1}-profile-raw.csv" \
+  | grep "\"" \
+  | sed -e "s/,/ /g" \
+  | sed -e 's/k/000/' \
+  | sed -e 's/M/000000/' \
+  | sed -e 's/^"[^\/]*\/\([0-9]\+\)\(\/[^\/]*\)*"/\1/' \
+  > ${1}-profile.csv
+#   | sed -e 's/^"[^\/]*\/\([0-9]\+\)(\/[^\/\"]*)*\/"/\1/' \
